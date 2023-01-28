ea731d37ab2555542a3cfeef7032d291969029fe
Naiden Boro <n.t.borodjiev@gmail.com>
2023-01-28 12:52:49 +0200

    Added a create location

---
+++ LandmarkHunt/Models/LocDTO.cs
@@ -0,0 +1,31 @@
+﻿using LandmarkHunt.Data;
+
+namespace LandmarkHunt.Models
+{
+    public class LocDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Latitude { get; set; } = string.Empty;
+        public string Longitude { get; set; } = string.Empty;
+        public string PhotoUrl { get; set; } = string.Empty;
+
+        public Location toLocation()
+        {
+            Location loc = new Location();
+            loc.Id = Id;
+            loc.Name = Name;
+            loc.Year = Year;
+            Latitude= Latitude.Replace(".",",");
+            Longitude = Longitude.Replace(".", ",");
+            loc.Latitude = double.Parse(Latitude);
+            loc.Longitude = double.Parse(Longitude);
+            loc.PhotoUrl= PhotoUrl;
+            return loc;
+
+        }
+    }
+
+}
+
---
+++ LandmarkHunt/wwwroot/js/LocationPicker/script.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+
+    $('#us2').locationpicker({
+        location: { latitude: 46.15242437752303, longitude: 2.7470703125 },
+        radius: 0,
+        inputBinding: {
+            latitudeInput: $('#Latitude'),
+            longitudeInput: $('#Longitude'),
+            locationNameInput: $('#location')
+        },
+        enableAutocomplete: true
+    });
+
+
+});
