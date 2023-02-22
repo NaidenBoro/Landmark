d1ea6c5ae7d89ec7fbdef99a2bd0f6da8ef8d6f3
Naiden Boro <n.t.borodjiev@gmail.com>
2023-02-22 19:35:54 +0200

    Revert "ok"
    This reverts commit 29834fe700962912a03a5df993482fafca06d96b, reversing
    changes made to e7834c7ef20f9e1088739b18aac473904aad62ea.

--- LandmarkHunt/Data/DbSeeder.cs
+++ LandmarkHunt/Data/DbSeeder.cs
@@ -19,17 +19,12 @@ namespace LandmarkHunt.Data
             }
             var user = new AppUser
             {
-<<<<<<< HEAD
                 UserName = "itkariera202223@gmail.com",
                 Email = "itkariera202223@gmail.com"
              
-=======
-                UserName = "itkariera2023@gmail.com",
-                Email = "itkariera2023@gmail.com"
->>>>>>> 5b0d336147082f75a9c05a7d27c10d331d18d032
             };
             if (userManager != null)
-            {
+           {
                 
                 var userInDb = await userManager.FindByEmailAsync(user.Email);
                 if (userInDb == null)
@@ -38,21 +33,11 @@ namespace LandmarkHunt.Data
                     await userManager.CreateAsync(user, "Itkariera2023_");
                    // await userManager.AddToRoleAsync(user, Roles.Admin.ToString());
 
-<<<<<<< HEAD
                 }
                 userInDb = await userManager.FindByEmailAsync(user.Email);
                 await userManager.AddToRoleAsync(userInDb!, Roles.Admin.ToString());
 
 
-=======
-
-                }
-
-                userInDb = await userManager.FindByEmailAsync(user.Email);
-                await userManager.AddToRoleAsync(userInDb!, Roles.Admin.ToString());
-
-
->>>>>>> 5b0d336147082f75a9c05a7d27c10d331d18d032
             }
         }
     }
