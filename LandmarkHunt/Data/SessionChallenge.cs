fdb0388e4a7f80c00628988e82ba4489fb3940a1
Naiden Boro <n.t.borodjiev@gmail.com>
2023-02-22 21:25:58 +0200

    added session class

---
+++ LandmarkHunt/Data/SessionChallenge.cs
@@ -0,0 +1,17 @@
+﻿using LandmarkHunt.Areas.Identity.Data;
+
+namespace LandmarkHunt.Data
+{
+    public class SessionChallenge
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string ChallengeId { get; set; } = string.Empty;
+        public virtual Challenge Challenge { get; set; } = new Challenge();
+        public string PlayerId { get; set; } = string.Empty;
+        public virtual AppUser Player { get; set; } = new AppUser();
+
+        public virtual ICollection<Location> Locations { get; set; } = new List<Location>();
+        public int Progress;
+    }
+}
