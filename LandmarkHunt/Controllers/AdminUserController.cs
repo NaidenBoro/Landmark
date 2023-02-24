0d15a24b956f003a05eab5c47ec32ae07b24b0cd
HristoKanev <icko_kanev@abv.bg>
2023-02-24 13:06:11 +0200

    added possibility for admin to change roles

---
+++ LandmarkHunt/Controllers/AdminUserController.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using LandmarkHunt.Areas.Identity.Data;
+using LandmarkHunt.Constants;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LandmarkHunt.Models;
+
+namespace LandmarkHunt.Controllers
+{
+    public class AdminUserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdminUserController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> ManageRoles()
+        {
+            
+        
+                var users = await _userManager.Users.ToListAsync();
+                var roles = await _roleManager.Roles.ToListAsync();
+                var model = new ManageRolesViewModel(_userManager)
+
+                {
+                    Users = users,
+                    Roles = roles
+                };
+            
+            return View("~/Views/AdminUser/ManageRoles.cshtml",model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ManageRoles(ManageRolesViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                    foreach (var userRole in model.UserRoles!)
+                {
+                    var user = await _userManager.FindByIdAsync(userRole.UserId);
+                    var currentRole = await _userManager.GetRolesAsync(user);
+                    if (currentRole.Any())
+                    {
+                        await _userManager.RemoveFromRoleAsync(user, currentRole.First());
+                    }
+                    if (!string.IsNullOrEmpty(userRole.RoleName))
+                    {
+                        await _userManager.AddToRoleAsync(user, userRole.RoleName);
+                    }
+                }
+                return RedirectToAction("ManageRoles");
+            }
+            return View("~/Views/AdminUser/ManageRoles.cshtml",model);
+        }
+    }
+}
---
+++ LandmarkHunt/Models/ManageRolesViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using LandmarkHunt.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace LandmarkHunt.Models
+{
+    
+    public class ManageRolesViewModel
+    {
+
+        
+        private readonly UserManager<AppUser>? _userManager;
+        public List<AppUser> Users { get; set; } = new List<AppUser>();
+        public List<IdentityRole> Roles { get; set; } = new List<IdentityRole>();
+        public List<UserRoleViewModel>? UserRoles { get; set; }
+
+        public ManageRolesViewModel(UserManager<AppUser>? userManager)
+        {
+            _userManager = userManager;
+            UserRoles = new List<UserRoleViewModel>();
+        }
+      public ManageRolesViewModel()
+       {
+        }
+
+        public SelectList RoleList
+        {
+            get
+            {
+                return new SelectList(Roles, "Name", "Name");
+            }
+        }
+
+        public async Task<string> GetUserRole(AppUser user)
+        {
+            if (_userManager != null)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                if (roles.Any())
+                {
+                    return roles.First();
+                }
+
+                else
+                {
+                    return "No Role";
+                }
+            }
+            else throw new Exception("_userManager is null.");
+        }
+    }
+
+    public class UserRoleViewModel
+    {
+        public string UserId { get; set; } = "";
+        public string RoleName { get; set; } = "";
+    }
+}
